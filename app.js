4d9c40d7b555b1d4c7e720a02af07810fdd88568
smit <smit51680@gmail.com>
2022-08-02 16:58:35 +0530

    TODOLIST-V2.0

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bodyparser=require("body-parser");
 const mongoose=require("mongoose");
 const { redirect } = require("express/lib/response");
 const _=require("lodash" );
+const res = require('express/lib/response');
 const app=express();
-// var items=[];
-// var workitems=[];
+
 
 //connect to database
 var url=process.env.URL;
@@ -22,13 +22,13 @@ const itemsSchema=new mongoose.Schema({
 const Item=mongoose.model("item",itemsSchema);
 
 const item1=new Item({
-    name:"samosa"
+    name:"+ button to insert"
 });
 const item2=new Item({
-    name:"Dabeli"
+    name:"checkbox to delete"
 });
 const item3=new Item({
-    name:"Vadapav"
+    name:"display items"
 });
 const defaultItems=[item1,item2,item3];
 
@@ -36,6 +36,7 @@ const ListSchema=new mongoose.Schema({
     name:String,
     listofitems:[itemsSchema]
 })
+
 //lists model is bascilly model which contains the all the lists and the items 
 const List=mongoose.model("List",ListSchema);
 
@@ -44,96 +45,40 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyparser.urlencoded({extended:true}));
 
-//for main todaylist
-var today=new Date();
-const options = { weekday: 'long', month: 'long', day: 'numeric' };
-var day=today.toLocaleDateString('en-IN',options);
+
+//for showing all lists
 app.get("/",function(req,res){
-   
-    Item.find({},function(err,items){
-        if(err){
-        } 
-        else{
-            if(items.length==0){
-                Item.insertMany(defaultItems,function(err){
-                    if(err){
-                        console.log("locha thaya che!!!");
-                    }
-                    else{
-                        console.log("Successfully inserted");
-                    }
-                })
-                res.redirect("/");
-            }
-            else{
-                res.render("list",{kindofday:day, items:items});
-            }
-        }
-       
+    List.find({name:{$ne:"Favicon.ico"}},function(err,items){
+          console.log(items);
+          res.render("home",{lists:items});
     });
-})
+}
+)
+
 
 //insert or adding
 app.post("/",function(req,res){
     var x=req.body.todo;
     const listname=req.body.list;
-    console.log(x);
-    console.log(listname);
     const newitem=new Item({
          name:x
     });
-    if(listname===day){
-        newitem.save();
-        res.redirect("/");
-    }
-    else{
-        List.findOne({name:listname},function(err,result){
+    List.findOne({name:listname},function(err,result){
                result.listofitems.push(newitem);
                result.save();
                res.redirect("/"+listname);
-        });
-    }
-   
-    // Item.insertMany(newitem,function(err){
-    //       if(!err){
-    //           console.log("added");
-    //           res.redirect("/");
-    //       }
-    // });
-    // console.log(req.body);
-    // if(req.body.list=="Work"){
-    //     console.log("ahiya ave che")
-    //     workitems.push(newitem);
-    //     res.redirect("/work");
-    // }
-    // else{
-    //     items.push(newitem);
-    //    
-    // } 
+    });
 })
 
 //deleting
 app.post("/delete",function(req,res){
      const id=req.body.deleted;
      const listname=req.body.listname;
-     if(listname===day){
-        Item.findByIdAndRemove(id,function(err){
-            if(!err){
-                console.log("deleted");
-               //  if(listname===today)
-                res.redirect("/");
-               //  else res.redirect("/"+listname);
-            }
-        });
-     }
-     else{
         List.findOneAndUpdate({name:listname},{$pull:{listofitems:{_id:id}}},function(err,result){
             if(!err){
                 res.redirect("/"+listname);
             }
-        });
-     }
-     
+        }); 
 })
 
 app.get("/:customListName",function(req,res){
@@ -152,13 +97,41 @@ app.get("/:customListName",function(req,res){
     })
 })
 
-// app.get("/work",function(req,res){
-//    res.render("list",{kindofday:"Work Items", items:workitems});
-// })
+app.post("/newlist",function(req,res){
+     var newtodoname=req.body.todo;
+     List.findOne({name:newtodoname},function(err,result){
+        if(!result){
+            const list=new List({
+                name:_.capitalize(newtodoname),
+                listofitems:defaultItems
+            })
+            list.save();
+            
+        }
+        res.redirect("/"+newtodoname);
+        
+    })
 
-// app.get("/about",function(req,res){
-//     res.render("about");
-// })
+});
+
+app.post("/openlist",function(req,res){
+    var todoid=req.body.listname;
+    console.log(todoid);
+    List.findById(todoid,function(err,result){
+        res.redirect("/"+result.name);
+    })
+    
+})
+
+app.post("/deletelist",function(req,res){
+    var todoid=req.body.listname;
+    List.findByIdAndRemove(todoid,function(err){
+        if(!err){
+            console.log("deleted");
+            res.redirect("/");
+        }
+    });
+})
 
 app.set('port', process.env.PORT || 3000);
 
